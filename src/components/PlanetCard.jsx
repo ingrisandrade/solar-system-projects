1f04fa8546d2900248187a88786b783e31c75fc1
Ingris Andrade <iac22@outlook.com>
2022-07-27 00:10:27 -0300

    add div com data-testid

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PlanetCard extends Component {
+  render() {
+    return (
+      <div data-testid="planet-card">
+        <p> </p>
+      </div>
+    );
+  }
+}
+
+export default PlanetCard;
