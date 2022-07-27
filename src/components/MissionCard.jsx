6b720fde5299d3ac78085d8f6d93b3a1ab4747bf
Ingris Andrade <iac22@outlook.com>
2022-07-27 01:00:22 -0300

    add quatro props

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropType from 'prop-types';
 
 class MissionCard extends Component {
   render() {
+    const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
-        <p> </p>
+        <p>{ name }</p>
+        <p>{ year }</p>
+        <p>{ country }</p>
+        <p>{ destination }</p>
       </div>
     );
   }
 }
+
+MissionCard.propType = {
+  name: PropType.string.isRequired,
+  year: PropType.number.isRequired,
+  country: PropType.string.isRequired,
+  destination: PropType.string.isRequired,
+};
 
 export default MissionCard;
