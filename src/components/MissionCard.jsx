5988339b4b279de4d0cbd35301b793bef2aed68a
Ingris Andrade <iac22@outlook.com>
2022-07-27 00:55:24 -0300

    add div com data-testid

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MissionCard extends Component {
+  render() {
+    return (
+      <div data-testid="mission-card">
+        <p> </p>
+      </div>
+    );
+  }
+}
+
+export default MissionCard;
